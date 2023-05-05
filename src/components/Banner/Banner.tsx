baa07634a4db63a61a897fc0a2f9d739538d8c55
Sergey Petrosyan <petrosyanserg33@gmail.com>
2023-05-05 12:10:44 +0400

    fix

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -16,7 +16,6 @@ export default function Banner() {
           Math.floor(Math.random() * request.data.results.length)
         ]
       );
-      await console.log(movie);
     }
 
     fetchData();
